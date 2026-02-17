dbcc98d4b3206317d7b2cb4751b0d40e4dd04b4c
Jonkkeyler333 <82979079+Jonkkeyler333@users.noreply.github.com>
2026-02-16 22:05:45 -0500

    fix(part3 phonebook) add the serving static content

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -5,6 +5,7 @@ const app = express()
 app.use(express.json())
 morgan.token('body', (req, res) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(express.static('dist'))
 
 let contacts = [
     { 
